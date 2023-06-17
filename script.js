c127e093f55ca312cc79e3e6d53a73405f77496c
linh <linh.ha.nguyen20@gmail.com>
2023-06-18 02:04:23 +0700

    add bot/user containers and profile

--- script.js
+++ script.js
@@ -2,39 +2,59 @@ function sendMessage() {
     var userInput = document.getElementById("user-input").value;
     var chatLog = document.getElementById("chat-log");
 
-    // Create message containers
-    var userMessageContainer = document.createElement("div");
-    var botMessageContainer = document.createElement("div");
-    botMessageContainer.classList.add("message-container");
-    userMessageContainer.classList.add("message-container");
-
     if (chatLog) {
-        // Create a chat message element
-        var userMessageElement = document.createElement("div");
-        userMessageElement.classList.add("chat-message");
-        userMessageElement.innerHTML = "<strong>You:</strong> ".concat(userInput);
+        // Create message containers
+        var userMessageContainer = createMessageContainer("user");
+        var botMessageContainer = createMessageContainer("bot");
+
+        // Create user message element
+        var userMessageElement = createChatMessageElement("user", userInput);
 
         // Append the user message to the message container
-        userMessageContainer.appendChild(userMessageElement);
-
-        // Append the message container to the chat log
+        addMessageToContainer(userMessageContainer, userMessageElement);
         chatLog.appendChild(userMessageContainer);
 
         // Process the user's message (you can replace this with your chatbot logic)
         var botResponse = "This is the chatbot's response.";
 
-        // Display chatbot response in the chat log
-        var botMessageElement = document.createElement("div");
-        botMessageElement.classList.add("chat-message");
-        botMessageElement.innerHTML = "<strong>Chatbot:</strong> ".concat(botResponse);
+        // Create bot response element
+        var botMessageElement = createChatMessageElement("bot", botResponse);
 
         // Append the bot response to the message container
-        botMessageContainer.appendChild(botMessageElement);
+        addMessageToContainer(botMessageContainer, botMessageElement);
         chatLog.appendChild(botMessageContainer);
     }
+
     // Clear the user input field
     var userInputField = document.getElementById("user-input");
     if (userInputField) {
         userInputField.value = "";
+    }
+}
+
+function createMessageContainer(profile) {
+    var messageContainer = document.createElement("div");
+    messageContainer.classList.add("message-container");
+    messageContainer.classList.add(profile + "-message");
+    return messageContainer;
+}
+
+function createChatMessageElement(profile, messageText) {
+    var chatMessageElement = document.createElement("div");
+    chatMessageElement.classList.add("chat-message");    
+    chatMessageElement.innerHTML = "<p>" + messageText + "</p>";
+    
+    return chatMessageElement;
+}
+
+function addMessageToContainer(container, message) {
+    if (container.classList.contains("bot-message")) {
+        var botProfile = document.createElement("img");
+        container.appendChild(botProfile);
+        container.appendChild(message);
+    } else if (container.classList.contains("user-message")) {
+        container.appendChild(message);
+        var userProfile = document.createElement("img");
+        container.appendChild(userProfile);
     }
 }
