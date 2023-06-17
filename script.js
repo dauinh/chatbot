5d1b4771bf7b597e2d596da970cb5a7b11e702db
linh <linh.ha.nguyen20@gmail.com>
2023-06-17 23:29:01 +0700

    refactor to separate files

---
+++ script.js
@@ -0,0 +1,22 @@
+function sendMessage() {
+    var userInput = document.getElementById("user-input").value;
+    var chatLog = document.getElementById("chat-log");
+    
+    // Display user message in the chat log
+    var userMessageElement = document.createElement("div");
+    userMessageElement.classList.add("chat-message");
+    userMessageElement.innerHTML = "<strong>You:</strong> " + userInput;
+    chatLog.appendChild(userMessageElement);
+
+    // Process the user's message (you can replace this with your chatbot logic)
+    var botResponse = "This is the chatbot's response.";
+    
+    // Display chatbot response in the chat log
+    var botMessageElement = document.createElement("div");
+    botMessageElement.classList.add("chat-message");
+    botMessageElement.innerHTML = "<strong>Chatbot:</strong> " + botResponse;
+    chatLog.appendChild(botMessageElement);
+
+    // Clear the user input field
+    document.getElementById("user-input").value = "";
+}
--- server.py
+++ server.py
@@ -5,16 +5,37 @@ import time
 hostName = "localhost"
 serverPort = 8080
 
-with open('index.html', 'r') as file:
-    html_string = file.read()
-
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
+        # Get the file path from the request URL
+        file_path = self.path[1:]  # Remove the leading slash
+
+        # Determine the file extension
+        file_extension = file_path.split('.')[-1].lower()
+
+        # Set the default content type to plain text
+        content_type = "text/plain"
+
+        # Check the file extension and set the appropriate content type
+        if file_extension == "html":
+            content_type = "text/html"
+        elif file_extension == "css":
+            content_type = "text/css"
+        elif file_extension == "js":
+            content_type = "application/javascript"
+
+        # Set the response headers
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
-        self.wfile.write(bytes(html_string, "utf-8"))
+        # Read and serve the file content
+        try:
+            with open(file_path, "rb") as file:
+                self.wfile.write(file.read())
+        except FileNotFoundError:
+            self.send_response(404)
+            self.wfile.write(b"File not found")
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
