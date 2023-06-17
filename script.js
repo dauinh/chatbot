d55555f27051ac8bee86c37e73ffe7cf819a8b84
linh <linh.ha.nguyen20@gmail.com>
2023-06-18 00:03:23 +0700

    add favicon. refactor back to js

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 function sendMessage() {
     var userInput = document.getElementById("user-input").value;
     var chatLog = document.getElementById("chat-log");
-    // Display user message in the chat log
-    var userMessageElement = document.createElement("div");
-    userMessageElement.classList.add("chat-message");
-    userMessageElement.innerHTML = "<strong>You:</strong> ".concat(userInput);
-    chatLog.appendChild(userMessageElement);
-    // Process the user's message (you can replace this with your chatbot logic)
-    var botResponse = "This is the chatbot's response.";
-    // Display chatbot response in the chat log
-    var botMessageElement = document.createElement("div");
-    botMessageElement.classList.add("chat-message");
-    botMessageElement.innerHTML = "<strong>Chatbot:</strong> ".concat(botResponse);
-    chatLog.appendChild(botMessageElement);
+    if (chatLog) {
+        // Display user message in the chat log
+        var userMessageElement = document.createElement("div");
+        userMessageElement.classList.add("chat-message");
+        userMessageElement.innerHTML = "<strong>You:</strong> ".concat(userInput);
+        chatLog.appendChild(userMessageElement);
+        // Process the user's message (you can replace this with your chatbot logic)
+        var botResponse = "This is the chatbot's response.";
+        // Display chatbot response in the chat log
+        var botMessageElement = document.createElement("div");
+        botMessageElement.classList.add("chat-message");
+        botMessageElement.innerHTML = "<strong>Chatbot:</strong> ".concat(botResponse);
+        chatLog.appendChild(botMessageElement);
+    }
     // Clear the user input field
-    document.getElementById("user-input").value = "";
+    var userInputField = document.getElementById("user-input");
+    if (userInputField) {
+        userInputField.value = "";
+    }
 }
